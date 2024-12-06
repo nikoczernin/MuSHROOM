e3beffe4f01c3121a279fe5891153f8e30d0fd71
GKapas1 <gergo.kapas12@gmail.com>
2024-12-06 19:50:11 +0100

    gpt working

--- GPT/gpt_testing.py
+++ GPT/gpt_testing.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 import openai
 from stanza.utils.conll import CoNLL
+import json
+import ast
 '''
 def load_conll_data(path):
     file = CoNLL.conll2doc(path)
@@ -16,18 +18,25 @@ with open(r"D:\School\NLP\NLP_Test1_key.txt", 'r') as file:
 
 client = openai.OpenAI(api_key=content)
 
-input=['Where is Austria located?']
-output=['Austria is located in America.']
+#data=json.load(open(r"data\preprocessed\sample_preprocessed.json"))
+data=[{"model_input":'Where is Austria located?',"model_output_text":'Australia is located in America.'},
+        {"model_input":'When was Kaiser Franz Josef born?',"model_output_text":'He was born in 2020.'}]
+spans=[]
 
-for i in range(len(input)):
+for i in range(len(data)):
     completion = client.chat.completions.create(
-        model="gpt-4o-mini", 
+        model="gpt-4o", 
         messages=[
             {"role": "system", "content": "You are an assistant tasked with finding errors in responses."},
             {
                 "role": "user",
-                "content": f'The question was "{input[i]}", and the response was "{output[i]}". Identify the parts of the answer that are incorrect or unsupported by the question. Output the hallucinated spans as a list of token ranges (start and end indices).'
+                "content": f'The question was "{data[i]['model_input']}", and the response was "{data[i]['model_output_text']}". Identify the parts of the answer that are incorrect or unsupported by the question. Answer with only the hallucinated spans as a list of token ranges (start and end indices), like in this example: "[(0,3),(8,20)]".'
             }
         ]
     )
-    print(completion.choices[0].message)
+    spans.append(ast.literal_eval(completion.choices[0].message.content))
+
+for i in range(len(data)):
+    print(f"\nQuestion: {data[i]['model_input']}\nAnswer: {data[i]['model_output_text']}\nHallucinated spans:")
+    for j in spans[i]:
+        print(data[i]['model_output_text'][j[0]:j[1]])
